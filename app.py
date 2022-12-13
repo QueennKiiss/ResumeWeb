18eebde563e1c7b5402933ec67dda8f4886dbc63
mmaestro <mmaestro@circontrol.com>
2022-12-13 18:22:41 +0100

    Feat: add app method with empty functions

---
+++ app.py
@@ -0,0 +1,12 @@
+import dash
+from dash import dcc, html
+import dash_bootstrap_components as dbc
+
+
+def main_app() -> None:
+    """ Function to be run when app execution"""
+    pass
+
+
+if __name__ == "__main__":
+    main_app()
