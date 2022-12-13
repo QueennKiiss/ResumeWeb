dd9f29f364ed52fcb371893b16444c1e812030f3
mmaestro <mmaestro@circontrol.com>
2022-12-13 18:49:41 +0100

    Feat: create layout file

---
+++ source/layout.py
@@ -0,0 +1,28 @@
+import dash
+from dash import Dash, dcc, html
+import dash_bootstrap_components as dbc
+
+
+def sidebar() -> html.Div:
+    """ Contains the layer of the left sidebar"""
+    div = html.Div()
+    return div
+
+
+def content_bar() -> html.Div:
+    """ Contains the layer of the content"""
+    div = html.Div()
+    return div
+
+
+def main_layout(app: Dash) -> html.Div:
+    """ Contains the outer layer of the layout"""
+    div = html.Div(
+        className="main_body",
+        children=[
+            sidebar(),
+            content_bar(),
+            ]
+        )
+
+    return div
