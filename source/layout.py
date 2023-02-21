f0a8e0af1e1354961ec5da4c34ba50c9f956e4f8
mmaestro <mmaestroizquierdo@gmail.com>
2023-02-21 15:13:13 +0100

    Feat: Add classname and empty children to main_layout children

--- source/layout.py
+++ source/layout.py
@@ -5,13 +5,19 @@ import dash_bootstrap_components as dbc
 
 def sidebar() -> html.Div:
     """ Contains the layer of the left sidebar"""
-    div = html.Div()
+    div = html.Div(
+        className="sidebar container",
+        children=[]
+        )
     return div
 
 
 def content_bar() -> html.Div:
     """ Contains the layer of the content"""
-    div = html.Div()
+    div = html.Div(
+        className="content container",
+        children=[]
+        )
     return div
 
 
