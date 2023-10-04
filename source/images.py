c11e484008a18637ba3330009a83cc20c6ea07bb
queenkiss <mmaestroizquierdo@gmail.com>
2023-10-04 17:20:15 +0200

    feat: update images links

--- source/images.py
+++ source/images.py
@@ -4,17 +4,16 @@ Contains all the constants with image paths
 
 IMAGES_PATH = "images"
 
-PROFILE_PHOTO = f"{IMAGES_PATH}/profile_image.png"
-SWEANTY_LOGO = f"{IMAGES_PATH}/logo_sweanty_1-184x81.png"
-USER_PROFILE = f"{IMAGES_PATH}/user_profile.png"
-GITHUB_ICON = f"{IMAGES_PATH}/icons8-github-375.png"
-CONTACT_ICON = f"{IMAGES_PATH}/icons8-contacts-375.png"
-HOME_ICON = f"{IMAGES_PATH}/icons8-home-375.png"
-LINKEDIN_ICON = f"{IMAGES_PATH}/icons8-linkedin-375.png"
-GMAIL_ICON = f"{IMAGES_PATH}/icons8-gmail-logo-375.png"
+USER_PROFILE = f"{IMAGES_PATH}/mmi_profile.png"
 
-UAB_LOGO = f"{IMAGES_PATH}/853197_logotip_logo1_x2-2352987532.png"
-UVA_LOGO = f"{IMAGES_PATH}/1623341387_597859_1623341692_noticia_normal_recorte2-2467531020.jpeg"
-USAL_LOGO = f"{IMAGES_PATH}/th-3720033856.jpeg"
+GITHUB_ICON = f"{IMAGES_PATH}/icons8-github-96.png"
+CONTACT_ICON = f"{IMAGES_PATH}/icons8-teléfono-96.png"
+HOME_ICON = f"{IMAGES_PATH}/icons8-casa-94.png"
+LINKEDIN_ICON = f"{IMAGES_PATH}/icons8-linkedin-94.png"
+GMAIL_ICON = f"{IMAGES_PATH}/icons8-gmail-64.png"
 
-COVER_PHOTO = f"{IMAGES_PATH}/m_suiza.jpg"
+UAB_LOGO = f"{IMAGES_PATH}/uab_logo.png"
+UVA_LOGO = f"{IMAGES_PATH}/uva_logo.jpeg"
+USAL_LOGO = f"{IMAGES_PATH}/usal_logo.jpeg"
+
+COVER_PHOTO = f"{IMAGES_PATH}/01717bed-e228-4fbb-9865-c0b51aea9dfb.jpg"
